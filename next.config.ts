550a86fffa388629f6cd794c8e2cf2bd20fe6eac
German Derbes Catoni <gderbes@nera-agro.com>
2024-11-27 17:48:18 -0300

    chore:first commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+/*
+La opción typedRoutes es una funcionalidad experimental introducida en versiones recientes de Next.js (13 o posterior).
+Propósito: Habilitar soporte tipado para rutas en aplicaciones que usan TypeScript.
+*/
+
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+};
+
+export default nextConfig;
